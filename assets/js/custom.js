abde69f55f1a355c90e4845da1b6c049c7f3cb36
sinistersloth <jaredxmilton@live.ca>
2016-04-04 19:59:34 +0100

    nearly done!

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,5 +8,32 @@
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
 
+        var sounds = [
+	
+	
+	
+	"./assets/img/sounds/orgasmfemale.wav",
+	"./assets/img/sounds/maleorgasm.wav",
+			  
+			  
+			  ],
+    oldSounds = [];
+
+var playSounds = function () {
+    var index = Math.floor(Math.random() * (sounds.length)),
+        thisSound = sounds[index];
+    
+        oldSounds.push(thisSound);
+        sounds.splice(index, 1);
+    
+        if (sounds.length < 1) {
+            sounds = oldSounds.splice(0, oldSounds.length);
+        }
+    
+$("#element").html("<audio autoplay><source src=\"" + thisSound + "\" type=\"audio\/mp3\"><\/audio>");
+}
+    
+    
+    
 		
 })(jQuery);
